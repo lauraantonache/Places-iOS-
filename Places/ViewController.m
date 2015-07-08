37ad72f26a3438460289a10eb05af0f4fa1189a7
iOS3 <ios3@serbans-mbp-2.ad.qualitance.com>
2015-07-08 16:29:55 +0300

    Done moving the view in relation with the keyboard.

--- Places/ViewController.m
+++ Places/ViewController.m
@@ -34,7 +34,6 @@
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint* redViewCenteredToButtonConstraint;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint* profileButtonWidthConstraint;
 
-
 @end
 
 @implementation ViewController
@@ -44,6 +43,89 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     _profile = [Profile new];
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(closeKeyboard)];
+    tapGesture.numberOfTapsRequired = 1;
+    [self.view addGestureRecognizer:tapGesture];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+    
+
+}
+
+bool isKeyBoardEnabled;
+CGRect frameEndUser ;
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+        double textPositionRelativeToKeyboard;
+    if (isKeyBoardEnabled )
+    {
+        if ([_lastName isFirstResponder])
+        {
+            textPositionRelativeToKeyboard =  _lastName.frame.origin.y + _lastName.frame.size.height
+            -  (self.view.frame.size.height - self.view.frame.origin.y- frameEndUser.size.height);
+        }
+        else if([_firstName isFirstResponder])
+        {
+            textPositionRelativeToKeyboard =  _firstName.frame.origin.y + _firstName.frame.size.height
+            -  (self.view.frame.size.height -self.view.frame.origin.y- frameEndUser.size.height);
+        }
+        
+        if(textPositionRelativeToKeyboard > 0 )
+        {
+            CGRect newFrame = self.view.frame;
+            newFrame.origin.y = -2*textPositionRelativeToKeyboard - 52;
+            
+            [self.view setFrame:newFrame];
+        }
+        else if (textPositionRelativeToKeyboard < -2*(_firstName.frame.size.height))
+        {
+            CGRect newFrame = self.view.frame;
+            newFrame.origin.y = 0;
+            
+            [self.view setFrame:newFrame];
+        }
+
+    }
+}
+
+-(void)keyboardWillShow: (NSNotification *) object
+{
+    isKeyBoardEnabled = YES;
+    NSDictionary* userInfo = [NSDictionary new];
+    userInfo = object.userInfo;
+    
+    frameEndUser = [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+ //   CGRect frame = [[userInfo[UIKeyboardFrameEndUserInfoKey]] CGRectValue];
+    double textPositionRelativeToKeyboard;
+    
+    if ([_lastName isFirstResponder])
+    {
+        textPositionRelativeToKeyboard =  _lastName.frame.origin.y + _lastName.frame.size.height
+        -  (self.view.frame.size.height - frameEndUser.size.height);
+    }
+    else if([_firstName isFirstResponder])
+    {
+        textPositionRelativeToKeyboard =  _firstName.frame.origin.y + _firstName.frame.size.height
+        -  (self.view.frame.size.height - frameEndUser.size.height);
+    }
+    
+    if(textPositionRelativeToKeyboard > 0 )
+    {
+        CGRect newFrame = self.view.frame;
+        newFrame.origin.y = -2*textPositionRelativeToKeyboard - 52;
+        
+        [self.view setFrame:newFrame];
+    }
+}
+
+-(void)keyboardWillHide: (id) object
+{
+    isKeyBoardEnabled = NO;
+    CGRect newFrame = self.view.frame;
+    newFrame.origin.y = 0;
+    
+    [self.view setFrame:newFrame];
 }
 
 - (IBAction)buttonPressed:(UIButton*)sender
@@ -56,10 +138,6 @@
             [_profileView setHidden:NO];
             [_settingsView setHidden:YES];
             self.redViewCenteredToButtonConstraint.constant = 0;
-            [UIView animateWithDuration:0.2 animations:^
-             {
-                 [self.view layoutIfNeeded];
-             }];
         }
         else if ([sender isEqual:_settingsButton])
         {
@@ -69,11 +147,12 @@
             [_settingsView setHidden:NO];
             //self.redViewCenteredToButtonConstraint.constant = 100;
             self.redViewCenteredToButtonConstraint.constant = _profileButton.frame.size.width;
-            [UIView animateWithDuration:0.2 animations:^
-             {
-            [self.view layoutIfNeeded];
-             }];
         }
+        
+        [UIView animateWithDuration:0.2 animations:^
+         {
+             [self.view layoutIfNeeded];
+         }];
     }
 }
 
@@ -167,12 +246,7 @@
 
 -(IBAction)touchedThePicture:(id)sender
 {
-     //= [UIActionSheet new];
-   // [actionSheet addButtonWithTitle:@"Take Photo"];
-   // [actionSheet addButtonWithTitles:@"Take Photo", @"Select Photo from Gallery", "Cancel", nil];
-    //[actionSheet addButtonWithTitle:@"Select Photo from Gallery"];
-    //[actionSheet cancelButtonTitle:@"Cancel"];
-    //[actionSheet addButtonWithTitle:@"Cancel"];
+
     
     UIActionSheet* actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:@"Take Photo", @"Select Photo from Gallery", nil];
     
@@ -200,6 +274,13 @@
     [self dismissViewControllerAnimated:YES completion:nil];
     _profile.image = info[UIImagePickerControllerOriginalImage];
     _profilePicture.image = _profile.image;
+}
+
+
+
+-(void) closeKeyboard
+{
+    [self.view endEditing:YES];
 }
 
 - (void)didReceiveMemoryWarning {
